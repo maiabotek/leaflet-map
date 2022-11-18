1948bbe895ad98aa353af9c576e90ad4de242b48
maiabotek <mbotek47@gmail.com>
2022-11-18 01:34:13 -0500

    updates

--- scripts/scriptMap2.js
+++ scripts/scriptMap2.js
@@ -1,5 +1,5 @@
 
-let map = L.map("map2").setView([21.431620, -75.051099], 5.4);
+let map = L.map("map2").setView([21.310922, -74.596825], 5.49);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/maiabotek/clakudly8000015ngo9pxcmt6/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFpYWJvdGVrIiwiYSI6ImNsYWt0cTN5NDBpcW0zb3BrNmt5NGhmZ3MifQ.Q8QeU6U3_UuTRwGRqcM8wA', {
     maxZoom: 18,
@@ -7,6 +7,6 @@ L.tileLayer('https://api.mapbox.com/styles/v1/maiabotek/clakudly8000015ngo9pxcmt
 }).addTo(map);
 
 for ( let i=0; 1 < places.length; i++) {
-  L.marker([places[i].latitude, places[i].long]) .bindPopup('<h3>'+places[i].name+'</h3>'+'<p>'+places[i].descr+'</p>')
+  L.marker([places[i].latitude,places[i].long ] ) .bindPopup('<h2>'+places[i].name+'</h2>'+'<p>'+places[i].descr+'</p>')
   .addTo(map);
 }
