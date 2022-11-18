6edc3e3d60a61d0c1726f18dd433ea4ff1d1ea07
maiabotek <mbotek47@gmail.com>
2022-11-18 16:55:42 -0500

    Update scriptMap2.js

--- scripts/scriptMap2.js
+++ scripts/scriptMap2.js
@@ -6,7 +6,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/maiabotek/clakudly8000015ngo9pxcmt
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
 }).addTo(map);
 
-for ( let i=0; 1 < places.length; i++) {
+for ( let i=0; i < places.length; i++) {
   L.marker([places[i].latitude,places[i].long ] ) .bindPopup('<h2>'+places[i].name+'</h2>'+'<p>'+places[i].descr+'</p>')
   .addTo(map);
 }
